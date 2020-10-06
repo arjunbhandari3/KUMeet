fe9a59920cb6382791c7442fee9a93d2e8e1cf69
Arjun Bhandari <arjunbhandari3773@gmail.com>
2020-10-06 10:52:49 +0545

    added urls

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -52,6 +52,17 @@ export default {
     getIceServer() {
         return {
             iceServers: [{
+                    urls: ["stun:eu-turn4.xirsys.com"]
+                },
+                {
+                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
+                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                    urls: [
+                        "turn:eu-turn4.xirsys.com:80?transport=udp",
+                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+                    ]
+                },
+                {
                     urls: [
                         'stun:stun.l.google.com:19302',
                         'stun:stun1.l.google.com:19302',
